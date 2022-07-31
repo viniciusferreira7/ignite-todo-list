7c1817718f1cb3c0a9da76c1a605d008fcad4896
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-31 19:55:02 -0300

    style(listempty): styled ListEmpty

--- src/components/ListEmpty/ListEmpty.styles.ts
+++ src/components/ListEmpty/ListEmpty.styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components'
 
-export const ListEmptyContainer = styled.main``
+export const ListEmptyContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 46rem;
+  margin: 0 auto;
+  margin-top: calc(3.69rem / 2);
+  padding: 4rem 1.5rem;
+  border-top: 2px solid ${(props) => props.theme.gray_500};
+
+  svg {
+    width: 3.5rem;
+    height: 3.5rem;
+    color: ${(props) => props.theme.gray_400};
+    margin-bottom: 1rem;
+  }
+
+  p {
+    color: ${(props) => props.theme.gray_300};
+    line-height: 1.4rem;
+  }
+
+  p:first-of-type {
+    font-weight: bold;
+  }
+`
